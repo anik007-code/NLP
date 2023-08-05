ded6180a1cf579bbd86231250969e8d2098f5e1b
Anik <aniklpu01@gmail.com>
2023-08-05 21:12:39 +0600

    initial push

---
+++ configs/config_data.py
@@ -0,0 +1,5 @@
+import os
+
+ROOT_DIR = os.getcwd()
+TRUE_DATA = "DATA/DATA_SET/TRUE/a1_True.csv"
+FAKE_DATA = "DATA/DATA_SET/FAKE/a2_Fake.csv"
---
+++ main.py
@@ -0,0 +1,3 @@
+from main_class.fake_news_class import FakeNews
+
+cls = FakeNews()
